41297a0e77b0bd8635f0a03fae599b1cd0a99d2e
Jason Brewer <jason@urawizard.com>
2019-03-22 18:25:44 +0000

    #2 Added axios to made a request to the desk api, added deskname and booked by on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
 import logo from './francesco.png';
+import axios from 'axios'
 import './App.css';
 
-class App extends Component {
+interface IState {
+  booked?: boolean;
+  userEmail?: string;
+  deskName?: string;
+}
+
+interface IProps {}
+
+class App extends React.Component<IProps, IState> {
+
+  constructor (props: any) {
+    super(props)
+    this.state = {
+      booked: false,
+      userEmail: '' ,
+      deskName: ''
+    }
+  }
 
   render() {
+
+    const id = '5629499534213120'
+
+    axios.get('https://arup-iot-desk.appspot.com/api/desks/' + id)
+    .then(response => {
+      console.log(response.data)
+      this.setState({
+        booked: response.data.booked,
+        userEmail: response.data.user_email,
+        deskName: response.data.name
+      })
+    })
 
     const hostname:string = window.location.hostname
 
     return (
       <div className="App">
         <header className="App-header">
+          <p>
+            <code>{hostname}</code>
+          </p>
+          <p>
+            <code>{this.state.deskName}</code>
+          </p>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hostname: <code>{hostname}</code>
+            Booked by: {this.state.userEmail}
+          </p>
+          <p>
+            Booked until 5:30pm
           </p>
         </header>
       </div>
