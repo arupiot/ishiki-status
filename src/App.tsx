b95ef1f33e2ed6d46cfc9243e51fc40d08a056b9
Jason Brewer <jason@urawizard.com>
2019-03-22 17:16:11 +0000

    #2 hostname is now displayed

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+
+    const hostname = window.location.hostname
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload?
+            Hostname: <code>{hostname}</code>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Reacts
-          </a>
         </header>
       </div>
     );
